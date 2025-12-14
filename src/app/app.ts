2ec1148d6e75ed0dcb51fe37eb4b455736aa9b8b
VI <vika.miroshnikova2016@gmail.com>
2025-12-14 22:15:09 +0300

    40 min ролика

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCard } from './common-ui/profile-card/profile-card';
+import { ProfileService } from './data/services/profile-service';
+import { JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  imports: [RouterOutlet, JsonPipe, ProfileCard],
+  template: `<pre>{{ data | json }}</pre>`,
+
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('itproger');
+  // protected readonly title = signal('itproger');
+
+  profileService = inject(ProfileService)
+  profiles: any = []
+
+  constructor() {
+    this.profileService.getTestAccounts()
+      .subscribe(val => {
+        this.profiles = val
+      })
+  }
 }
